d84de397deeff1b0d99eca192af410cbb9da2c59
rongfengliang <1141591465@qq.com>
2019-03-26 14:54:32 +0800

    add uuid function demo

--- template/init.tpl
+++ template/init.tpl
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "CONSUL_ADDRESS = ${consul_address}" > /tmp/iplist
 mkdir -p  /opt/data/pg
+
+{
+  "name":"${name}",
+  "age": "${age}",
+  "platform":"${platform}",
+  "id":"${uuid()}"
+}
--- template/main.tf
+++ template/main.tf
@@ -2,9 +2,18 @@ data "template_file" "init" {
   template = "${file("init.tpl")}"
   vars = {
     consul_address = "http://localhost:8500"
+    name = "dalongdemo"
+    age =444
+    platform ="mobile"
   }
 }
+data "local_file" "foo" {
+    filename = "${path.module}/token.ini"
+}
+
+
+// base64  && format function demo
 resource "local_file" "foo" {
-    content     = "${data.template_file.init.rendered}"
+    content  = "${base64encode(format("%s\n%s",data.template_file.init.rendered, data.local_file.foo.content))}" 
     filename = "${path.module}/init.sh"
 }
